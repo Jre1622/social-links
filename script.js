dc34a07657c2a90bb5b5f00198d7c1dc257674fc
userName <useraccount@userNames-MacBook-Pro.local>
2024-02-25 20:32:36 +0700

    ricky tan

---
+++ script.js
@@ -0,0 +1,20 @@
+let locationElement = document.querySelector(".card-header a");
+console.log(locationElement);
+
+colors = ["red", "blue", "green", "yellow", "purple", "orange", "pink", "brown"];
+
+let scaleUp = true;
+
+function changeColorsAndSize() {
+  locationElement.style.color = colors[Math.floor(Math.random() * colors.length)];
+
+  if (scaleUp) {
+    locationElement.style.transform = "scale(1.2)";
+    scaleUp = false;
+  } else {
+    locationElement.style.transform = "scale(1)";
+    scaleUp = true;
+  }
+}
+
+setInterval(changeColorsAndSize, 500); // change color and size every 500 milliseconds
